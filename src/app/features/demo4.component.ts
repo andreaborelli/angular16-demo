4a7c140d391fd3fa128e5983142d0882db79b301
Andrea Borelli <andreaborelli@blu.it>
2024-06-19 17:57:44 +0200

    reactive form rxjs and async pipe

--- src/app/features/demo4.component.ts
+++ src/app/features/demo4.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
@@ -10,31 +10,25 @@ import { Meteo } from '../model/meteo';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   template: `
-   <input type="text" placeholder="Search City" [formControl]="input">
-    <h1 *ngIf="meteo">Temperature: {{meteo.main.temp}}°</h1>
-    <pre>{{meteo | json}}</pre>
+     <input type="text" placeholder="Search City" [formControl]="input">
+     <h1>Temperature: {{(meteo$ | async)?.main?.temp}}°</h1>
+      <!-- <pre>{{meteo | json}}</pre> -->
   `,
   styles: [
   ]
 })
 export class Demo4Component {
 
-  input = new FormControl('');
-
-    meteo: Meteo | undefined;
-
-   constructor(private http: HttpClient) {
-     this.input.valueChanges
+  input = new FormControl('')
+  http = inject(HttpClient)
+  meteo$ = this.input.valueChanges
        .pipe(
          debounceTime(700), /* filtra emissione se un valore è un tempo inferiore a quello impostato 700ms per emettere un valore,
          imposta un ritardo di 700 millisecondi. Ciò significa che l'Observable emetterà un valore solo se non ci sono altre emissioni nei successivi 700 millisecondi.*/
          mergeMap(text => this.http.get<Meteo>(`http://api.openweathermap.org/data/2.5/weather?q=${text}&units=metric&APPID=eb03b1f5e5afb5f4a4edb40c1ef2f534`))
        )
            // .subscribe(console.log);
-
-       .subscribe(res => {
-         this.meteo = res;
-       })
-   }
+           /* sequenza di observable che diventa un unico observable non emette il valore del campo di input
+           ma il valore generato dalla chiamata http get*/
 
 }
