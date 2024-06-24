d4b80abe965d5db4146d4514e9064e713d04af94
andreaborelli <webdesigner@blu.it>
2024-06-24 18:48:24 +0200

    toObservable - from signal to observable

--- src/app/features/demo4.component.ts
+++ src/app/features/demo4.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { toSignal } from '@angular/core/rxjs-interop'
+import { toObservable, toSignal } from '@angular/core/rxjs-interop'
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { debounceTime, mergeMap } from 'rxjs';
+import { catchError, debounceTime, filter, mergeMap, of } from 'rxjs';
 import { Meteo } from '../model/meteo';
 
 @Component({
@@ -11,40 +11,57 @@ import { Meteo } from '../model/meteo';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   template: `
-     <input type="text" placeholder="Search City" [formControl]="input">
-     <h1 *ngIf="meteo()">Temperature: {{meteo()?.main?.temp}}°</h1>
+     <input
+      type="text"
+      placeholder="Search City"
+      (input)="onChangeText($event)"
+      [value]="city()"
+    >
   <br>
   <br>
-      <button (click)="input.setValue('Trieste')">Trieste</button>
-      <button (click)="input.setValue('Milano')">Milano</button>
-      <button (click)="input.setValue('Palermo')">Palermo</button>
-      
-       <!-- (click)="input.setValue('Roma')" valorizza campo di input con il valore 'Roma' -->
+      <button (click)="city.set('Trieste')">Trieste</button>
+      <button (click)="city.set('Milano')">Milano</button>
+      <button (click)="city.set('Palermo')">Palermo</button>
+     <h1 *ngIf="meteo$ | async as meteo">Temperature: {{meteo?.main?.temp}}°</h1>
 
-      <!-- <pre>{{meteo() | json}}</pre> -->
-      <!-- <pre>{{meteo | json}}</pre> -->
+       <!-- (click)="city.set('Roma')" valorizza campo di input con il valore 'Roma' -->
+
+      <!-- <pre>{{city() | json}}</pre> -->
+        <!-- <pre>{{city()}}</pre> -->
   `,
   styles: [
   ]
 })
 export class Demo4Component {
 
-  input = new FormControl('')
+  // converto un signal in un observable per poter sfruttare tutti gli operatori di rxjs che ci fanno comodo
+
+  city = signal('') // signal che rappresenta il campo di input
   http = inject(HttpClient)
 
-  // signal che dipende da un observable utlizzando la funzionalità toSignal di angular 16
-  meteo = toSignal( // questa funzione converte un observable in un signal
-    this.input.valueChanges
-       .pipe(
-         debounceTime(700), /* filtra emissione se un valore è un tempo inferiore a quello impostato 700ms per emettere un valore,
-         imposta un ritardo di 700 millisecondi. Ciò significa che l'Observable emetterà un valore solo se non ci sono altre emissioni nei successivi 700 millisecondi.*/
-         mergeMap(text => this.http.get<Meteo>(`http://api.openweathermap.org/data/2.5/weather?q=${text}&units=metric&APPID=eb03b1f5e5afb5f4a4edb40c1ef2f534`))
-       )
-  )
+  meteo$ = toObservable(this.city) // converto il signal in un observable
+      .pipe(
+        filter(text => text.length > 3), // filtro i valori che hanno una lunghezza maggiore di 2
+        debounceTime(1000),
+      mergeMap(
+        text => this.http.get<Meteo>(`http://api.openweathermap.org/data/2.5/weather?q=${text}&units=metric&APPID=eb03b1f5e5afb5f4a4edb40c1ef2f534`)
+          .pipe(
+            catchError(e => of(null)) // rigenera un observable con of che emette un valore nullo in caso di errore
+          )
+      ),
+      )
+    // .subscribe(value =>{ // sottoscrivo l'observable
+    // console.log(value)
+    // })
 
 
-           // .subscribe(console.log);
-           /* sequenza di observable che diventa un unico observable non emette il valore del campo di input
-           ma il valore generato dalla chiamata http get*/
+  onChangeText(event: Event) {
+    const input = event.currentTarget as HTMLInputElement;
+    this.city.set(input.value)
+  }
+
+
+    // un observable qundo va in errore smette di funzionare e non emette più valori, per questo si usa catchError
+
 
 }
