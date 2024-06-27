bdb7ea97d20c8e22acdab7c68ffc89ddab29a1b7
andreaborelli <webdesigner@blu.it>
2024-06-27 11:43:51 +0200

    Route Params, Input Setter and reactivity

--- src/app/features/demo5.component.ts
+++ src/app/features/demo5.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-demo5',
@@ -9,7 +10,8 @@ import { CommonModule } from '@angular/common';
     <p>
       demo5 works!
     </p>
-    <h1>{{title}}</h1>
+    <!-- <h1>{{title}}</h1> -->
+    {{title$ | async}}
   `,
   styles: [
   ]
@@ -22,6 +24,24 @@ quindi possiamo decorare una proprietà */
 
 // inserendo un secondo parametro ' withComponentInputBinding() ' nel file app.config.ts
 
-  @Input() title: string = '';
+
+/* in angular possiamo utilizzare il decoratore input anche come setter,
+vuol dire che questa funzione sarà invocata
+ogni qual volta che passiamo una nuova proprietà a un componente,
+indipendentemente che questa sia un componente di route,
+o un componente normale a cui passiamo degli input */
+
+/* le differenze rispetto ad un input classico
+è che possiamo avviare un'azione nel momento
+in cui cambia una proprietà in input,
+come una chiamata al server,
+o semplicemente rendere reattiva questa proprietà es: val */
+
+  title$ = new BehaviorSubject(''); // BehaviorSubject è un observable che emette un valore iniziale
+
+  @Input() set title(val: string) {
+    this.title$.next(val); // next emette un nuovo valore
+    console.log(val);
+  }
 
 }
